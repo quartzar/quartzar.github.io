986d574caadc56de06fc95265e6901cc98fe2b98
quartzar <joey.dorn7@gmail.com>
2023-01-31 16:31:04 +0000

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
+  // target: 'static',
   // ssr: false,
   // router: {
   //   base: '/quartzar.github.io/'
