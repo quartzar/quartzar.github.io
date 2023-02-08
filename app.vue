c1574938fc234694835d1581abd98be961e8428a
quartzar <joey.dorn7@gmail.com>
2023-02-08 15:16:57 +0000

    added social icons at base of page

--- app.vue
+++ app.vue
@@ -1,29 +1,39 @@
 <template>
   <!-- <div class="all:transition-400 "> -->
-    <div id="app">
+  <div id="app">
     <!-- <div md:pt-100> -->
     <!-- class="md:pt-10" -->
     <Html lang="en" />
     <Meta property="og:image" content="/cover.jpg" />
     <Meta name="twitter:card" content="summary_large_image" />
-    
+
 
     <!-- Navbar -->
-    <Navbar2 class="animate-slide-in-down animate-count-1 ease-in-out"/>
+    <Navbar2 />
+    <!-- class="animate-slide-in-down animate-count-1 ease-in-out"/> -->
 
 
     <!-- Rest of Page -->
-    <div class="scroller md:pt-28 md:pb-18 min-h-screen bg-fixed bg-zinc-900">
-          <!-- /* style="url('/background.png')"> */ -->
+    <div class="scroller md:pt-28 md:pb-5 min-h-screen bg-fixed bg-zinc-900">
+      <!-- /* style="url('/background.png')"> */ -->
 
-      <NuxtPage/>
-        <!-- class="rounded-2xl shadow-[0px_0px_60px_-15px_rgba(0,0,0,0.3)] shadow-indigo-600 border border-indigo-600"> -->
+      <NuxtPage />
+      <!-- class="rounded-2xl shadow-[0px_0px_60px_-15px_rgba(0,0,0,0.3)] shadow-indigo-600 border border-indigo-600"> -->
       <!-- </NuxtPage> -->
-      
+
+      <div class="relative grid justify-center place-self-center pt-5">
+        <a href="https://github.com/quartzar" title="GitHub">
+        <i class="fa fa-github github" id="github"/>
+        </a>
+      </div>
+
     </div>
   </div>
 </template>
 
+<style>
+@import url("https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+</style>
 
 
 <!-- <template>
@@ -41,6 +51,7 @@
 ::-webkit-scrollbar-track:hover {
   background: transparent;
 }
+
 ::-webkit-scrollbar-track {
   background: red;
 }
