fcccba8c8656b0933d3e994b16754ea09ff20afb
quartzar <joey.dorn7@gmail.com>
2023-02-10 21:58:03 +0000

    typo

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <header class="mt-10 mx-auto max-w-screen-x1 px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-32 relative">
+        <header class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-32 relative">
             <div class="text-center relative">
                 <h1 class="font-rubik font-bold text-yellow-200 text-4xl sm:text-5xl md:text-6xl glow-yellow-txt">
                     Joseph Dorn
