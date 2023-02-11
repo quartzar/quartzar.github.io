f450860f726399e2917cdb2c666e15f5af883faf
quartzar <joey.dorn7@gmail.com>
2023-02-11 12:35:25 +0000

    switched back to md content for homepage with components

--- components/content/HomeHeader.vue
+++ components/content/HomeHeader.vue
@@ -1,15 +1,28 @@
+<!-- This is useful if using YAML, and instead of lot, using {{ title }} -->
+<!-- <script setup>
+defineProps({
+    title: {
+        type: String,
+        default: 'Default Title'
+    },
+    description: {
+        type: String,
+        default: 'Default Description'
+    }
+})
+</script> -->
+
 <template>
     <div>
         <header class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-32 relative">
             <div class="text-center relative">
                 <h1 class="font-rubik font-bold text-yellow-200 text-4xl sm:text-5xl md:text-6xl glow-yellow-txt">
-                    Joseph Dorn
+                    <slot name="title"/>
                 </h1>
                 <p class="mt-3 max-w-md mx-auto font-ubuntu text-cyan-100 sm:text-lg md:mt-5 md:text-xl md:max-w-2xl glow-cyan-txt">
-                    Welcome to my website! Here you can find some projects I'm working on, with blogs coming soon<sup>TM</sup>. I'm still working on this site, so it's a bit bare at the moment. Enjoy your stay!
+                    <slot name="description"/>
                 </p>
             </div>
-        
         </header>
     </div>
 </template>
